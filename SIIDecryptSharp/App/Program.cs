e3849c1bad8e179b483441e94cdad04c4eeb0e20
jammerxd <josh@menzelstudios.com>
2023-04-26 11:30:08 -0500

    1.0.1 release

--- SIIDecryptSharp/App/Program.cs
+++ SIIDecryptSharp/App/Program.cs
@@ -8,8 +8,8 @@ namespace App
         {
             Console.WriteLine("Hello, World!");
 
-            var raw = Decryptor.Decrypt(Path.Combine(Directory.GetCurrentDirectory(), "game.1.47.sii"));
-            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "testing.1.47.txt"), System.Text.Encoding.UTF8.GetString(raw));
+            var raw = Decryptor.Decrypt(Path.Combine(Directory.GetCurrentDirectory(), "game.1.47.0.sii"));
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "testing.1.47.0.txt"), System.Text.Encoding.UTF8.GetString(raw));
         }
     }
 }
